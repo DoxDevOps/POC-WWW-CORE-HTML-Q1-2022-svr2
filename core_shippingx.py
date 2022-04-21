21c8b60ae000b89ff20bd61c65f003bbb77c2f6b
Billz <58287516+billz94@users.noreply.github.com>
2022-04-21 20:26:27 +0200

    Update core_shippingx.py

--- core_shippingx.py
+++ core_shippingx.py
@@ -81,7 +81,7 @@ for site_id in cluster['site']:
             
             version = msg.format(result).strip()
             
-            core_version = "v5.1.1"
+            core_version = "v5.1.2"
             
             if core_version == version:
                 msgx = "Hi there,\n\nDeployment of Core to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF/LIN HIS."
@@ -107,7 +107,7 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of CORE to v5.1.1 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF/LIN HIS."
+                    msg = "Hi there,\n\nDeployment of CORE to v5.1.2 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF/LIN HIS."
                     params = {
                         "api_key": os.getenv('API_KEY'),
                         "recipient": recipient,
